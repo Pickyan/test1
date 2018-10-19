80dd5d4d17c5cb0a0b6ad0aa891ddd4d96f053af
Pickyan <yzy18339407951@163.com>
2018-10-19 19:44:22 +0800

    添加主函数

--- download.py
+++ download.py
@@ -8,3 +8,10 @@ def download(url,user_agent):
 	except requests.exceptions.RequestsException as e:
 		print("download error:",e.reason)
 	return html
+
+
+if __name__ == '__main__':
+	url = input("请输入您要下载的网页地址：")
+	headers = input("请输入user-agent:")
+	html-date = download(url,user_agent)
+	print(html)
